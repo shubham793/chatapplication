189d33ec3fc0bac35acc8c72f3dc91daa2b8e925
shubham793 <80848003+shubham793@users.noreply.github.com>
2021-07-08 22:30:22 +0530

    initial commit

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const LoginForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const authObject = {
+      "project-ID": "b7e7aecf-d24f-4646-93c2-afe9a357eb4c",
+      "User-Name": username,
+      "User-Secret": password,
+    };
+    try {
+      await axios.get("https://api.chatengine.io/chats", {
+        headers: authObject,
+      });
+      localStorage.setItem("username", username);
+      localStorage.setItem("password", password);
+      window.location.reload();
+      setError("");
+    } catch (error) {
+      setError("Oops user not found");
+    }
+  };
+  return (
+    <div className="wrapper">
+      <div className="form">
+        <h1 className="title">Chat Application</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="input"
+            placeholder="Username"
+            required
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="input"
+            placeholder="password"
+            required
+          />
+          <div align="center">
+            <button type="submit" className="button">
+              <span>start chatting</span>
+            </button>
+          </div>
+        </form>
+        <h2 className="error">{error}</h2>
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
